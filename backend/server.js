8bd219934944d43fb1389c4a00d09979706b7be9
candesisini <candelariasisini@gmail.com>
2025-06-10 15:34:13 -0300

    configure base de datos y organice archivosen diferentes carpetas

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const taskRoutes = require('./routes/tasks.routes');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,15 @@ app.use(express.static('../frontend/public'));
 
 // Rutas
 app.use('/api', taskRoutes);
+
+// Add SQLite database setup
+const db = new sqlite3.Database('./database.sqlite', (err) => {
+  if (err) {
+    console.error('Error opening database ' + err.message);
+  } else {
+    console.log('Connected to the SQLite database.');
+  }
+});
 
 // Iniciar servidor
 app.listen(PORT, () => {
